72f5b5834127f42685ef099905293e9169dcac35
Nicolas Bohorquez <nicolas.bohorquez@alephsa.com>
2017-08-10 22:14:33 -0500

    Improved design

---
+++ js/script.js
@@ -0,0 +1,357 @@
+//LOADER/SPINNER
+$(window).bind("load", function() {
+
+    "use strict";
+    
+    $(".spn_hol").fadeOut(1000);
+});
+
+
+//MENU APPEAR AND HIDE
+$(document).ready(function() {
+
+    "use strict";
+    
+    $(window).scroll(function() {
+
+        "use strict";
+        
+        if ($(window).scrollTop() > 80) {
+            $(".navbar").css({
+                'margin-top': '0px',
+                'opacity': '1'
+            })
+            $(".navbar-nav>li>a").css({
+                'padding-top': '15px'
+            });
+            $(".navbar-brand img").css({
+                'height': '35px'
+            });
+            $(".navbar-brand img").css({
+                'padding-top': '0px'
+            });
+            $(".navbar-default").css({
+                'background-color': 'rgba(59, 59, 59, 0.7)'
+            });
+        } else {
+            $(".navbar").css({
+                'margin-top': '-100px',
+                'opacity': '0'
+            })
+            $(".navbar-nav>li>a").css({
+                'padding-top': '45px'
+            });
+            $(".navbar-brand img").css({
+                'height': '45px'
+            });
+            $(".navbar-brand img").css({
+                'padding-top': '20px'
+            });
+            $(".navbar-default").css({
+                'background-color': 'rgba(59, 59, 59, 0)'
+            });
+        }
+    });
+});
+
+
+
+
+ // MENU SECTION ACTIVE
+$(document).ready(function() {
+
+    "use strict";
+    
+    $(".navbar-nav li a").click(function() {
+
+        "use strict";
+        
+        $(".navbar-nav li a").parent().removeClass("active");
+        $(this).parent().addClass("active");
+    });
+});
+
+
+
+// Hilight MENU on SCROLl
+
+$(document).ready(function() {
+
+    "use strict";
+    
+    $(window).scroll(function() {
+
+        "use strict";
+        
+        $(".page").each(function() {
+
+            "use strict";
+            
+            var bb = $(this).attr("id");
+            var hei = $(this).outerHeight();
+            var grttop = $(this).offset().top - 70;
+            if ($(window).scrollTop() > grttop - 1 && $(window).scrollTop() < grttop + hei - 1) {
+                var uu = $(".navbar-nav li a[href='#" + bb + "']").parent().addClass("active");
+            } else {
+                var uu = $(".navbar-nav li a[href='#" + bb + "']").parent().removeClass("active");
+            }
+        });
+    });
+});
+
+
+
+//SMOOTH MENU SCROOL
+
+
+$(function() {
+	
+	"use strict";
+
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+
+// FIX HOME SCREEN HEIGHT
+$(document).ready(function() {
+
+    "use strict";
+    
+    setInterval(function() {
+
+        "use strict";
+        
+        var widnowHeight = $(window).height();
+        var containerHeight = $(".home-container").height();
+        var padTop = widnowHeight - containerHeight;
+        $(".home-container").css({
+            'padding-top': Math.round(padTop / 2) + 'px',
+            'padding-bottom': Math.round(padTop / 2) + 'px'
+        });
+    }, 10)
+});
+
+
+
+//PARALLAX
+$(document).ready(function() {
+
+    "use strict";
+    
+    $(window).bind('load', function() {
+        "use strict";
+        parallaxInit();
+    });
+
+    function parallaxInit() {
+        "use strict";
+        $('.home-parallax').parallax("30%", 0.1);
+        $('.subscribe-parallax').parallax("30%", 0.1);
+        $('.testimonial').parallax("10%", 1);
+        /*add as necessary*/
+    }
+});
+
+
+
+//OWL CAROSEL
+$(document).ready(function() {
+
+    "use strict";
+    
+    $("#owl-demo").owlCarousel({
+        autoPlay: 3000,
+        items: 4, //10 items above 1000px browser width
+        itemsDesktop: [1370, 3], //5 items between 1000px and 901px
+        itemsDesktopSmall: [900, 2], // betweem 900px and 601px
+        itemsTablet: [600, 1], //2 items between 600 and 0
+    });
+});
+
+
+    
+ //PRETTYPHOTO
+
+$(document).ready(function() {
+
+    "use strict";
+
+    $("a[rel^='prettyPhoto']").prettyPhoto({
+        show_title: false,
+        /* true/false */
+    });
+});
+
+
+
+//WOW JS
+$(document).ready(function() {
+
+    "use strict";
+ 
+    new WOW().init();
+});
+
+
+
+//RESPONSIVE VIDEO
+$(document).ready(function() {
+
+    "use strict";
+    
+    // Basic FitVids Test
+    $(".video").fitVids();
+});
+
+
+
+/// SMOOTH SCROLL           
+
+$(document).ready(function() {
+
+    "use strict";
+    
+    var scrollAnimationTime = 1200,
+        scrollAnimation = 'easeInOutExpo';
+    $('a.scrollto').bind('click.smoothscroll', function(event) {
+        event.preventDefault();
+        var target = this.hash;
+        $('html, body').stop().animate({
+            'scrollTop': $(target).offset().top
+        }, scrollAnimationTime, scrollAnimation, function() {
+            window.location.hash = target;
+        });
+    });
+    //COUNTER
+    $('.counter_num').counterUp({
+        delay: 10,
+        time: 2000
+    });
+});
+
+
+//COUNT DOWN TIMER
+$(document).ready(function(){
+	//###################################################################
+		// Author: ricocheting.com
+		// Version: v3.1
+		// Date: 2017-01-03
+		// Description: displays the amount of time until the "dateFuture" entered below.
+
+		var CDown = function() {
+			this.state=0;// if initialized
+			this.counts=[];// array holding countdown date objects and id to print to {d:new Date(2013,11,18,18,54,36), id:"countbox1"}
+			this.interval=null;// setInterval object
+		}
+
+		CDown.prototype = {
+			init: function(){
+				this.state=1;
+				var self=this;
+				this.interval=window.setInterval(function(){self.tick();}, 1000);
+			},
+			add: function(date,id){
+				this.counts.push({d:date,id:id});
+				this.tick();
+				if(this.state==0) this.init();
+			},
+			expire: function(idxs){
+				for(var x in idxs) {
+					this.display(this.counts[idxs[x]], "Now!");
+					this.counts.splice(idxs[x], 1);
+				}
+			},
+			format: function(r){
+				var pre='',post='',divide=', ',
+					out="";
+				if(r.d != 0){out += pre+r.d +" "+((r.d==1)?"dia":"dias")+post+divide;}
+				if(r.h != 0){out += pre+r.h +" "+((r.h==1)?"hora":"horas")+post+divide;}
+				out += pre+r.m +" "+((r.m==1)?"min":"mins")+post+divide;
+				out += pre+r.s +" "+((r.s==1)?"seg":"segs")+post+divide;
+
+				return out.substr(0,out.length-divide.length);
+			},
+			math: function(work){
+				var	y=w=d=h=m=s=ms=0;
+
+				ms=(""+((work%1000)+1000)).substr(1,3);
+				work=Math.floor(work/1000);//kill the "milliseconds" so just secs
+
+				y=Math.floor(work/31536000);//years (no leapyear support)
+				w=Math.floor(work/604800);//weeks
+				d=Math.floor(work/86400);//days
+				work=work%86400;
+
+				h=Math.floor(work/3600);//hours
+				work=work%3600;
+
+				m=Math.floor(work/60);//minutes
+				work=work%60;
+
+				s=Math.floor(work);//seconds
+
+				return {y:y,w:w,d:d,h:h,m:m,s:s,ms:ms};
+			},
+			tick: function(){
+				var now=(new Date()).getTime(),
+					expired=[],cnt=0,amount=0;
+
+				if(this.counts)
+				for(var idx=0,n=this.counts.length; idx<n; ++idx){
+					cnt=this.counts[idx];
+					amount=cnt.d.getTime()-now;//calc milliseconds between dates
+
+					// if time is already past
+					if(amount<0){
+						expired.push(idx);
+					}
+					// date is still good
+					else{
+						this.display(cnt, this.format(this.math(amount)));
+					}
+				}
+
+				// deal with any expired
+				if(expired.length>0) this.expire(expired);
+
+				// if no active counts, stop updating
+				if(this.counts.length==0) window.clearTimeout(this.interval);
+				
+			},
+			display: function(cnt,msg){
+				document.getElementById(cnt.id).innerHTML=msg;
+			}
+		};
+
+		window.onload=function(){
+			var cdown = new CDown();
+
+			cdown.add(new Date(2017,7,18,10,0,0), "countbox1");
+		};
+});
+
+
+
+//VIDEO BACKGROUND
+$(document).ready(function() {
+  var videobackground = new $.backgroundVideo($('body'), {
+    "align": "centerXY",
+    "width": 1280,
+    "height": 720,
+    "path": "media/",
+    "filename": "cloud",
+    "types": ["mp4","ogg","webm"]
+  });
+});
